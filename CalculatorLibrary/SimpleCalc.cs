ded5fa6d2357f05c7ce989bfdf41a8882055294f
rasheenruwisha <A00229247@mycambrian.ca>
2020-12-09 03:36:23 +0530

    Second section

---
+++ CalculatorLibrary/SimpleCalc.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace CalculatorLibrary
+{
+    public class SimpleCalc
+    {
+
+        public double addNumbers(double leftNumber, double rightNumber)
+        {
+            double result = double.NaN;
+            result = leftNumber + rightNumber;
+            return result;
+        }
+
+        public double substractNumbers(double leftNumber, double rightNumber)
+        {
+            double result = double.NaN;
+            result = leftNumber - rightNumber;
+            return result;
+        }
+
+
+        public double mulitplyNumbers(double leftNumber, double rightNumber)
+        {
+            double result = double.NaN;
+            result = leftNumber * rightNumber;
+            return result;
+        }
+
+        public double divideNumbers(double leftNumber, double rightNumber)
+        {
+            double result = double.NaN;
+            if (rightNumber != 0)
+            {
+                result = leftNumber / rightNumber;
+            }
+            return result;
+        }
+
+
+
+        public double OperatorSwitch(double leftNumber, double rightNumber, string op)
+        {
+
+            double result = double.NaN;
+
+            switch (op)
+            {
+                case "+":
+                    result = addNumbers(leftNumber, rightNumber);
+                    break;
+                case "-":
+                    result = substractNumbers(leftNumber, rightNumber);
+                    break;
+                case "*":
+                    result = mulitplyNumbers(leftNumber, rightNumber);
+                    break;
+                case "/":
+                    result = divideNumbers(leftNumber, rightNumber);
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+    }
+}
--- ExamWeekAssignments/Controllers/CalculatorController.cs
+++ ExamWeekAssignments/Controllers/CalculatorController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CalculatorLibrary;
 using ExamWeekAssignments.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,7 +12,9 @@ namespace ExamWeekAssignments.Controllers
 {
     public class CalculatorController : Controller
     {
-        // GET: /<controller>/
+
+        SimpleCalc simpleCalc = new SimpleCalc();
+
         public IActionResult Index()
         {
             return View();
@@ -22,42 +25,17 @@ namespace ExamWeekAssignments.Controllers
         [ActionName("Index")]
         public IActionResult IndexPost([Bind("leftNumber,rightNumber,operat")] CalculatorModel calculator)
         {
+
             CalculatorModel cm = new CalculatorModel();
             cm.rightNumber = calculator.rightNumber;
             cm.leftNumber = calculator.leftNumber;
             cm.operat = calculator.operat;
-            cm.result = calculationResult(calculator.leftNumber, calculator.rightNumber, calculator.operat);
+            cm.result = simpleCalc.OperatorSwitch(calculator.leftNumber, calculator.rightNumber, calculator.operat);
 
             return View("Index", cm);
         }
 
 
-        public double calculationResult(double leftNumber, double rightNumber, string op)
-        {
-
-            double result = double.NaN;
-
-            switch (op)
-            {
-                case "+":
-                    result = leftNumber + rightNumber;
-                    break;
-                case "-":
-                    result = leftNumber - rightNumber;
-                    break;
-                case "*":
-                    result = leftNumber * rightNumber;
-                    break;
-                case "/":
-                    if (rightNumber != 0)
-                    {
-                        result = leftNumber / rightNumber;
-                    }
-                    break;
-                default:
-                    break;
-            }
-            return result;
-        }
+        
     }
 }
